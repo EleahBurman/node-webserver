0b6920a75ed21556c11fbc55dd8152d7509b97a1
eleahburman <eleahjburman@gmail.com>
2023-12-20 15:45:14 -0500

    Stub up users router

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const express = require('express');
 const app = express();
 //define port
 const port = 3000;
-//app is listening on a port
+//create router object
+const usersRouter = express.Router();
+usersRouter.post('/', function (req, res){
+
+});
+usersRouter.get('/:id', function(req, res){
+
+});
+
+//anything with a users router now has /users as the home page
+app.use('/users', usersRouter)
+//app is listening on a port and server has started
 app.listen(port, () => {
-    console.log('Server started');
+    console.log('Listening on port', port);
 })
+//now we can use the app in other scripts as well
+module.exports = app;
